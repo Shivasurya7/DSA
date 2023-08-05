3153944c58952affa8cabe1b0f2819bfda77c2a9
Shivasurya7 <128982338+Shivasurya7@users.noreply.github.com>
2023-08-05 22:03:05 +0530

    05\08\2023

--- Arrays/Hard/56 Merge Intervals.cpp
+++ Arrays/Hard/56 Merge Intervals.cpp
@@ -41,5 +41,24 @@ public:
     }
 };
 
+//optimal approach
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector <vector<int>> ans;
+        for(int i = 0; i< intervals.size(); i++){
+            if(ans.empty() or  intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
+
 
 
