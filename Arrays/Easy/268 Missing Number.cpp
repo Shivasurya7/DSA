a966b6d77f062bfd2f4ad62d74ed3d3dd8b4af6f
Shivasurya7 <128982338+Shivasurya7@users.noreply.github.com>
2023-08-08 06:48:12 +0530

    Update 268 Missing Number.cpp

--- Arrays/Easy/268 Missing Number.cpp
+++ Arrays/Easy/268 Missing Number.cpp
@@ -89,15 +89,18 @@ Space Complexity: O(1) as we are not using any extra space.
 
 */
 
-int missingNumber(vector<int>&a, int N) {
-    int xor1 = 0, xor2 = 0;
-    for (int i = 0; i < N - 1; i++) {
-        xor2 = xor2 ^ a[i]; 
-        xor1 = xor1 ^ (i + 1); 
+class Solution {
+public:
+    int missingNumber(vector<int>& a) {
+        int xor1 = 0, xor2 = 0;
+        for (int i = 0; i < a.size(); i++) {
+            xor2 = xor2 ^ a[i]; 
+            xor1 = xor1 ^ i; 
+        }
+        xor1 = xor1 ^ a.size(); 
+        return (xor1 ^ xor2);
     }
-    xor1 = xor1 ^ N; 
-    return (xor1 ^ xor2); 
-}
+};
 
 
 
